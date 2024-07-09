8b02257cf63f3af1d26176bb1118f3ddf56889d4
NaveenkumarMD <naveen9715568487@gmail.com>
2024-07-09 22:07:19 +0530

    fix : update unsplash api for wallpapers

--- Src/Components/Wallpaper/wallpaper.js
+++ Src/Components/Wallpaper/wallpaper.js
@@ -1,53 +1,88 @@
-const mainelement = document.getElementById("main")
-const shuffleelement = document.getElementById("shuffle")
-const selectwallpaperelement=document.getElementById("select-wallpaper")
+import { createApi } from "unsplash-js";
+
+const mainelement = document.getElementById("main");
+const shuffleelement = document.getElementById("shuffle");
+const selectwallpaperelement = document.getElementById("select-wallpaper");
 //-----------------------------------------------------
 //get a random unsplash image with tag nature
 //-----------------------------------------------------
 
-var currwallpapertag="nature"
-const wallpapertag=localStorage.getItem("wallpapertag")
-if(wallpapertag){
-    currwallpapertag=wallpapertag
-    getwallpaper()
+const api = createApi({
+  accessKey: "57-h-ID7kXYDAIERv4hZskagvVtiN7myPI3rJXSE3io",
+});
+var currwallpapertag = "nature";
+const wallpapertag = localStorage.getItem("wallpapertag");
+if (wallpapertag) {
+  currwallpapertag = wallpapertag;
+  getwallpaper();
 }
+// `https://source.unsplash.com/photos/random/1920x1080/?${currwallpapertag}`
+function getwallpaper() {
+  // const url = `https://api.unsplash.com/photos/random/?client_id=57-h-ID7kXYDAIERv4hZskagvVtiN7myPI3rJXSE3io`;
+  // let urlmodified = new URL(url);
+  // console.log(urlmodified);
+  // fetch(url)
+  //   .then((response) => {
+  //     console.log(response);
+  //     return response.json();
+  //   })
+  //   .then((blob) => {
+  //     console.log(blob);
+  //     const objectURL = URL.createObjectURL(blob);
+  //     mainelement.style.background = `url(${objectURL})`;
+  //     mainelement.style.cssText += `
+  //       height:100vh;
+  //       background-repeat: none;
+  //       background-size: cover;
+  //       background-position: center;
+  //       background-attachment: fixed;
+  //       color:var(--text-color)
+  //       `;
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 
-function getwallpaper(){
-    fetch(`https://source.unsplash.com/random/1920x1080/?${currwallpapertag}`)
-    .then(response => response.blob())
-    .then(blob => {
-        const objectURL = URL.createObjectURL(blob);
-        mainelement.style.background = `url(${objectURL})`
-        mainelement.style.cssText +=
-        `
+  api.photos
+    .getRandom({
+      orientation: "landscape",
+      count: 1,
+      query: currwallpapertag,
+    })
+    .then((res) => {
+      const response = res.response;
+      const imageLink = response[0].urls.full;
+      mainelement.style.background = `url(${imageLink})`;
+      mainelement.style.cssText += `
         height:100vh;
         background-repeat: none;
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
         color:var(--text-color)
-        `
+        `;
+      console.log("set");
     })
+
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-
-
 shuffleelement.addEventListener("click", () => {
-    getwallpaper()
-})
+  getwallpaper();
+});
 
 //---------------------------------------------------------------
 //set wallpaper tag
 //---------------------------------------------------------------
-var wallpapertaginput
-selectwallpaperelement.addEventListener("click",()=>{
-    wallpapertaginput=window.prompt("Enter the wallaper keyword")
-    localStorage.setItem("wallpapertag",wallpapertaginput)
-    currwallpapertag=wallpapertaginput
-    getwallpaper()
-})
-
-
+var wallpapertaginput;
+selectwallpaperelement.addEventListener("click", () => {
+  wallpapertaginput = window.prompt("Enter the wallaper keyword");
+  localStorage.setItem("wallpapertag", wallpapertaginput);
+  currwallpapertag = wallpapertaginput;
+  getwallpaper();
+});
 
 // var hours_so_far=localStorage.getItem("hours_so_far") || 0
 // const hours_so_far_node=document.getElementById("hours_so_far")
